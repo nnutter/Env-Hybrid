8abe56faa5393b22aeb5a8f6c86f5a2930dbea89
Nathaniel Nutter <iam@nnutter.com>
2014-05-09 15:15:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Env::Hybrid - It's new $module
+
+# SYNOPSIS
+
+    use Env::Hybrid;
+
+# DESCRIPTION
+
+Env::Hybrid is ...
+
+# LICENSE
+
+Copyright (C) Nathaniel Nutter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nathaniel Nutter <iam@nnutter.com>
---
+++ lib/Env/Hybrid.pm
@@ -0,0 +1,39 @@
+package Env::Hybrid;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Env::Hybrid - It's new $module
+
+=head1 SYNOPSIS
+
+    use Env::Hybrid;
+
+=head1 DESCRIPTION
+
+Env::Hybrid is ...
+
+=head1 LICENSE
+
+Copyright (C) Nathaniel Nutter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nathaniel Nutter E<lt>iam@nnutter.comE<gt>
+
+=cut
+
