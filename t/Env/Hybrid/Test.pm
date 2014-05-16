cff42f01f39170087e5482efe57d239a50ea5e0e
Nathaniel Nutter <iam@nnutter.com>
2014-05-16 17:08:10 -0500

    add test infrastructure

---
+++ t/Env/Hybrid/Test.pm
@@ -0,0 +1,42 @@
+package Env::Hybrid::Test;
+use parent 'Env::Hybrid';
+
+use strict;
+use warnings;
+
+use Config qw(%Config);
+use File::Spec qw();
+use File::Temp qw(tempdir);
+use List::MoreUtils qw(uniq);
+use YAML::Syck qw(DumpFile LoadFile);
+
+my @vars;
+sub vars { @vars }
+sub relative_path { 'config.yaml' }
+sub load_file { shift; LoadFile(shift) }
+
+sub init {
+    my $class = shift;
+    my $test_data = { @_ };
+
+    my @ivars;
+
+    for my $k (keys %{$test_data->{ENV}}) {
+        push @ivars, $k;
+        $ENV{$k} = $test_data->{ENV}{$k};
+    }
+
+    my @data = ($test_data->{HOME}, @{$test_data->{DIRS}});
+    my @dirs = map { tempdir() } @data;
+    for (my $i = 0; $i < @data; $i++) {
+        my $tempfilename = File::Spec->join($dirs[$i], relative_path());
+        push @ivars, keys %{$data[$i]};
+        DumpFile($tempfilename, $data[$i]);
+    }
+    $ENV{ENV_CONFIG_HOME} = shift @dirs;
+    $ENV{ENV_CONFIG_DIRS} = join($Config{path_sep}, @dirs);
+
+    @vars = uniq @ivars;
+}
+
+1;
